d48abf97b36db3eadc6f07fb9a3e692101b34f11
Joe O'Regan <joeaoregan@yahoo.ie>
2020-09-14 02:20:09 +0100

    Lobby buttons

--- Classes/Lobby.cpp
+++ Classes/Lobby.cpp
@@ -23,4 +23,28 @@ void Lobby::setupUI() {
 	logo->setAnchorPoint(Vec2(0.5f, 0.5f));
 	logo->setPosition(Vec2(visibleSize.width * 0.5f, visibleSize.height * 0.75f));
 	this->addChild(logo);
+
+	ui::Button* soloButton = ui::Button::create();
+	soloButton->setAnchorPoint(Vec2(0.5f, 0.5f));
+	soloButton->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height * 0.4f));
+	soloButton->loadTextures("soloButton.png", "soloButtonPressed.png");
+	soloButton->addTouchEventListener(CC_CALLBACK_2(Lobby::soloPressed, this));
+	this->addChild(soloButton);
+	
+	ui::Button* duoButton = ui::Button::create();
+	duoButton->setAnchorPoint(Vec2(0.5f, 0.75f));
+	duoButton->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height * 0.25f));
+	duoButton->loadTextures("duoButton.png", "duoButtonPressed.png");
+	duoButton->addTouchEventListener(CC_CALLBACK_2(Lobby::duoPressed, this));
+	this->addChild(duoButton);
+}
+
+void Lobby::soloPressed(Ref *pSender, ui::Widget::TouchEventType eEventType)
+{
+
+}
+
+void Lobby::duoPressed(Ref *pSender, ui::Widget::TouchEventType eEventType)
+{
+
 }
--- Classes/Lobby.h
+++ Classes/Lobby.h
@@ -2,6 +2,7 @@
 #define __DOODLER_LOBBY__
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
 
 class Lobby : public cocos2d::Node
 {
@@ -12,6 +13,8 @@ protected:
 	bool init() override;
 	void onEnter() override;
 	void setupUI();
+	void soloPressed(cocos2d::Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
+	void duoPressed(cocos2d::Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
 };
 
 #endif /* defined(__DOODLER_LOBBY__) */
